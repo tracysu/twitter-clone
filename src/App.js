73d90c81e185179654fe03512ad57ec20d1ed633
tracysu <50897148+tracysu@users.noreply.github.com>
2023-06-21 19:27:25 -0400

    react beginning files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Building a Twitter Clone w/ Tiffany, Gustavo, and Tracy!🚀</h1>
+    </div>
+  );
+}
+
+export default App;
