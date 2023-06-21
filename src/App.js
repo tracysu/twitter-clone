d15b4138deca5962263bcad0005c999761b36d47
tracysu <50897148+tracysu@users.noreply.github.com>
2023-06-21 19:54:49 -0400

    Twitter icon and materials ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
-    <div className="App">
+    //BEM 
+    <div className="app">
       <h1>Building a Twitter Clone w/ Tiffany, Gustavo, and Tracy!🚀</h1>
+    
+      {/* Sidebar */}
+      <Sidebar />
+
+      {/* Feed */}
+
+      {/* Widgets */}
     </div>
   );
 }
---
+++ src/Sidebar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import './Sidebar.css'
+import TwitterIcon from '@mui/icons-material/Twitter';
+
+function Sidebar(){
+    return (
+        <div className="sidebar">
+            {/* Twitter icon */}
+            <TwitterIcon />
+
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            
+            {/* Button -> Tweet */}
+
+        </div>
+    );
+}
+
+export default Sidebar; 
