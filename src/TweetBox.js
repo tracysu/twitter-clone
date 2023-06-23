0b79327b5aeadf9d284c100e5e6e4efb9fe829f4
tracysu <50897148+tracysu@users.noreply.github.com>
2023-06-22 22:12:06 -0400

    centered the tweet box

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -6,16 +6,19 @@ import { Avatar, Button } from "@mui/material";
 function TweetBox() {
   return (
     <div className='tweetBox'>
-        <div className='tweetBox__input'>
-            <Avatar src=
-            "https://e1.pxfuel.com/desktop-wallpaper/399/852/desktop-wallpaper-aesthetic-pfp-cartoon-pfp.jpg"
-            />
-            <input placeholder="What's happening?" type='text'/>
-            <input placeholder="Enter image URL" type='text'/>
-        </div>
-        <Button>Tweet</Button>
+        <form>
+            <div className='tweetBox__input'>
+                <Avatar src=
+                "https://e1.pxfuel.com/desktop-wallpaper/399/852/desktop-wallpaper-aesthetic-pfp-cartoon-pfp.jpg"
+                />
+                <input placeholder="What's happening?" type='text'/>
+            </div>                
+            <input className="tweetBox_imageInput" 
+                placeholder=" Optional: Enter image URL" type='text'/>
+            <Button className='tweetBox__tweetButton'>Tweet</Button>
+        </form>
     </div>
-  )
+  );
 }
 
 export default TweetBox
